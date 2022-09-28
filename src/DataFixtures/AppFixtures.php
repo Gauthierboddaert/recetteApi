ffe075b0d8d3d32ff7de92abb852cf1b03199e63
gboddaert <gboddaert@insitaction.com>
2022-09-28 20:09:34 +0200

    fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,16 +8,28 @@ use App\Entity\User;
 use App\Enum\EnumCategory;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+
+    private $userPasswordHasher;
+    
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        
+
+        $faker = Factory::create("fr-FR");
         $user = new User();
-        $user->setEmail("Boddaert.Gauthier@gmail.com");
-        $user->setPassword("test");
-        $user->setUsername('Gauthier');
+        $user->setEmail($faker->name()."@gmail.com");
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, "coucou"));
+        $user->setUsername($faker->name());
         for($i = 0;$i<=3;$i++){
             $category = new Category();
             $category->setType(EnumCategory::VIANDE);
@@ -26,8 +38,8 @@ class AppFixtures extends Fixture
          
         for($i=0;$i<10; $i++){
             $recette = new Recette();
-            $recette->setTitle("mon titre .$i");
-            $recette->setDescription("ma description .$i");
+            $recette->setTitle($faker->text(10));
+            $recette->setDescription($faker->text(100));
             $recette->setUser($user);
             $recette->setCategoryPlat($category);
             $manager->persist($recette);
