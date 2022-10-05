59161ab0eaff7c674120063ced16ed1c9c6d56a1
gboddaert <gboddaert@insitaction.com>
2022-10-05 19:20:38 +0200

    fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -25,15 +25,21 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-
         $faker = Factory::create("fr-FR");
+
         $user = new User();
-        $image = new Image();
-        $image->setPath("/root/rere");
-        $user->setEmail("g@gmail.com");
-        $user->setRoles(['PUBLIC_ACCESS']);
+        $user->setRoles(['IS_AUTHENTICATED_ANONYMOUSLY']);
+        $user->setEmail("user@gmail.com");
         $user->setPassword($this->userPasswordHasher->hashPassword($user, "coucou"));
         $user->setUsername($faker->name());
+        $manager->persist($user);
+
+        $admin = new User();
+        $admin->setRoles(['IS_AUTHENTICATED_FULLY']);
+        $admin->setEmail("admin@gmail.com");
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, "coucou"));
+        $admin->setUsername($faker->name());
+        
         for($i = 0;$i<=3;$i++){
             $category = new Category();
             $category->setType(EnumCategory::VIANDE);
@@ -44,8 +50,10 @@ class AppFixtures extends Fixture
             $recette = new Recette();
             $recette->setTitle($faker->text(10));
             $recette->setDescription($faker->text(100));
-            $recette->setUser($user);
+            $recette->setUser($admin);
             $recette->setCategoryPlat($category);
+            $image = new Image();
+            $image->setPath("C:\\Users\\BODDAERTGauthier\\Desktop\\recetteApi\\public\\upload\\ham.jpg");
             $recette->addImage($image);
            
             $manager->persist($recette);
